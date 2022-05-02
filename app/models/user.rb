6aac1288a12a40c17e2f59356ea47e653a2920a9
TeamAbella060420 <jabella@rscm.com.sa>
2022-05-02 21:44:11 +0300

    Create user.rb

---
+++ app/models/user.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.name.split(' ')[0] # assuming the user model has a name
+      user.last_name = auth.info.name.split(' ')[1]
+      # user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+  # called by the devise registrations controller
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if (data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info'])
+        user.email = data['email'] if user.email.blank?
+      end
+    end
+  end
+
+  def friends
+    friends_array = friendships.map { |f| f.friend if f.confirmed }
+    friends_array.concat(inverse_friendships.map { |f| f.user if f.confirmed })
+    friends_array.compact.uniq!
+  end
+
+  def pending_friends
+    friendships.map { |f| f.friend unless f.confirmed }.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find { |f| f.user == user }
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friendship.include? user
+  end
+
+  def unfriend(user)
+    table = friendships.where(requested_id: user.id).or(inverse_friendships.where(requestor_id: user.id)).ids
+    Friendship.delete(table.first)
+  end
+
+  # Option 1: Purely overriding the #as_json method
+  def as_json(options={})
+    { first: self.first_name,  last: self.last_name }  # NOT including the email field
+  end
+end
